cd0b942dc38ed026384c7df4638d1234e4f4aeab
System Administrator <root@MBP-di-Danilo.station>
2022-04-21 01:15:49 +0200

    ERC20 basic implementation

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,9 +3,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MyToken is ERC20 {
+
+
     constructor(uint256 initialSupply) ERC20("Dughito Token", "DUG") {
         _mint(msg.sender, initialSupply);
     }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        SafeERC20.safeTransfer(this, to, amount);
+        return true;
+    }
+
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,12 @@
-var MyToken = artifacts.require("./MyToken.sol");
+var MyToken = artifacts.require("MyToken.sol");
+var MyTokenSale = artifacts.require("MyTokenSale");
 
 //deployer è l'handle per accedere alla blockchain
+//deploy richiama il costruttore degli smart contract
 module.exports = async function(deployer) {
+  let addr = await web3.eth.getAccounts();
   await deployer.deploy(MyToken, 10000000);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+  let instance = MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, 1000000);
 };
